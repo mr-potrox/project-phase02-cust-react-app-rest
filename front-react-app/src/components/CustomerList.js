48b9c592ffe8c783a5790421f7e5735fd6045f44
JhonattanDiazLM <78913799+JhonattanDiazLM@users.noreply.github.com>
2024-03-08 14:45:41 -0500

    adding some validations and css

--- front-react-app/src/components/CustomerList.js
+++ front-react-app/src/components/CustomerList.js
@@ -2,32 +2,36 @@ import React from 'react';
 
 export default function CustomerList(props){
   return (
-    <div className="boxed" >
-        <h4>Customer List</h4>
-        <table id="customer-list">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Pass</th>
-            </tr>
-          </thead>
-          <tbody>
-            { 
-              props.customers.map(function(customer) {
-                return (<tr key={customer.id}
-                  className={ (customer.id === props.formObject.id )?'selected': ''}
-                  onClick={()=>props.handleListClick(customer)} 
-                  >
-                    <td>{customer.name}</td>
-                    <td>{customer.email}</td>
-                    <td>{customer.password}</td>
-                  </tr>);
-              })
-            }
-          </tbody>
-        </table>
-    </div>
+    <React.Fragment>
+      <h2>React App + REST Server</h2>
+      <div className="boxed" >
+          <h4>Customer List</h4>
+          <table id="customer-list">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Pass</th>
+              </tr>
+            </thead>
+            <tbody>
+              { 
+                props.customers.map(function(customer) {
+                  return (<tr key={customer.id}
+                    className={ (customer.id === props.formObject.id )?'selected': ''}
+                    onClick={()=>props.handleListClick(customer)} 
+                    >
+                      <td>{customer.name}</td>
+                      <td>{customer.email}</td>
+                      <td>{customer.password}</td>
+                    </tr>);
+                })
+              }
+            </tbody>
+          </table>
+      </div>
+    </React.Fragment>
+    
   );
 }
   
